7a164a7ebf31c7347c14a9e0373de61b8bb54014
Hannah Malm <hannahemalm616@gmail.com>
2021-01-18 23:15:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
-
+class
+Book
+ 
+  def initialize(title)
+  @title = title 
+  end
+   
+  def title
+  @title 
+  end 
+  
+  def author=(author)
+    @author = author
+    def author
+      @author
+    end   
+  end 
+end 
 
