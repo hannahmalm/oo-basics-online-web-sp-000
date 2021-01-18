bdb4392af07648bcc354327e79a9294aadc91d7d
Hannah Malm <hannahemalm616@gmail.com>
2021-01-18 23:36:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,48 @@
 # Make your shoe class here!
 
+class Shoe
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def brand
+    @brand
+  end
+ 
+  def color=(color)
+    @color = color
+  end
+ 
+  def color
+    @color
+  end
+ 
+  def size=(num)
+    @size = num
+  end
+ 
+  def size
+    @size
+  end
+ 
+  def material=(material)
+    @material = material
+  end
+ 
+  def material
+    @material
+  end
+  
+  
+  
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+
+end
+
+
+ book = Book.new("Some Title")
+  book.turn_page
