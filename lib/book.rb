30bca755a342d2d2c2f359d87d355e1a7fc2cd3e
Hannah Malm <hannahemalm616@gmail.com>
2021-01-18 23:27:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,28 +1,36 @@
-class
-Book
+class Book
  
   def initialize(title)
-  @title = title 
+    @title = title
   end
-   
+ 
   def title
-  @title 
-  end 
-  
+    @title
+  end
+ 
   def author=(author)
     @author = author
-    def author
-      @author
-    end   
-  end 
-   
-  def page_count =(num)
-      @page_count = num 
-    def page_count
-      @page_count
-    end
-  end 
-  
-end   
-  
+  end
+ 
+  def author
+    @author
+  end
+ 
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+ 
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+ 
+end
 
